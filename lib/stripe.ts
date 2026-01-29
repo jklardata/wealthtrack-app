2ddfd09cd5706bffb612e4715109c938fa0a20de
Justin Leu <157411852+jklardata@users.noreply.github.com>
2026-01-28 18:13:33 -0600

    Update Stripe API version to 2026-01-28.clover

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -6,7 +6,7 @@ const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
 export const stripe = stripeSecretKey
   ? new Stripe(stripeSecretKey, {
-      apiVersion: '2025-12-15.clover',
+      apiVersion: '2026-01-28.clover',
       typescript: true,
     })
   : (null as unknown as Stripe);
